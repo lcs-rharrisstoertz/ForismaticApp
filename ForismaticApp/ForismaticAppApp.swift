4ad4b1823f548b70b7ca6285b3d39274c56c7637
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-02-19 11:51:49 -0500

    Initial Commit

---
+++ ForismaticApp/ForismaticAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForismaticAppApp.swift
+//  ForismaticApp
+//
+//  Created by Harris-Stoertz, Rowan on 2021-02-19.
+//
+
+import SwiftUI
+
+@main
+struct ForismaticAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
